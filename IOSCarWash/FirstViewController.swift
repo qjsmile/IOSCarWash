b06777fce590e48a37b5ebc72a7b342562946e93
莲莲 <cflflyl@lianlian.local>
2015-03-18 18:56:36 +0800

    Initial Commit

---
+++ IOSCarWash/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IOSCarWash
+//
+//  Created by 莲莲 on 15-3-18.
+//  Copyright (c) 2015年 www.cfl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
